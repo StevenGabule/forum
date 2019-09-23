6db8d419c0c001335a97d600cc420a2690107e6a
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-09-24 01:24:20 +0800

    implement the complete slug problem

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -66,8 +66,7 @@ class ThreadsController extends Controller
             'user_id' => auth()->id(),
             'channel_id' => request('channel_id'),
             'title' => request('title'),
-            'body' => request('body'),
-            'slug' => request('title')
+            'body' => request('body')
         ]);
 
         return redirect($thread->path())->with('flash', 'Your thread has been published!');
