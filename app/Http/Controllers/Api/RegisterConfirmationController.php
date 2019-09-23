fb9220ebf6c6d516d087fe69e5ba8ecdee01e8dc
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-09-24 00:29:09 +0800

    refactor the email confirmation and the registration section

--- app/Http/Controllers/Api/RegisterConfirmationController.php
+++ app/Http/Controllers/Api/RegisterConfirmationController.php
@@ -9,12 +9,11 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))->firstOrFail()->confirm();
-        } catch (\Exception $e) {
+        $user = User::where('confirmation_token', request('token'))->first();
+        if (!$user) {
             return redirect(route('threads'))->with('flash', 'Unknown token.');
         }
+        $user->confirm();
         return redirect('/threads')->with('flash', 'Your account is now confirmed! You may post to the forum!');
-
     }
 }
